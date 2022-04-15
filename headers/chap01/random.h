536d63e277f673251f2c9ddf786620d1efff517e
ClazyChen <clazychen@gmail.com>
2022-04-15 15:42:59 +0800

    intro lab 3 (sum of array)

---
+++ headers/chap01/random.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "random_framework.h"
+
+namespace clazy {
+
+// 给定向量的长度
+// 生成一个随机的向量
+// 1. 如果给定的参数是int，则生成随机的0~65535
+// 2. 如果给定的参数是double，则生成随机的0~1
+template <typename T = int>
+requires (is_same_v<T, int> || is_same_v<T, double>)
+class RandomVector : public clazy_framework::Algorithm<vector<T>, int> {
+private:
+    constexpr static bool isInt = is_same_v<T, int>;
+    constexpr static bool isDouble = is_same_v<T, double>;
+public:
+    vector<T> apply(int n) override {
+        vector<T> v(n);
+        for (int i = 0; i < n; i++) {
+            if constexpr (isInt) {
+                v[i] = clazy_framework::Random::nextUInt16();
+            } else if constexpr (isDouble) {
+                v[i] = clazy_framework::Random::nextDouble();
+            }
+        }
+        return v;
+    }
+};
+
+// 生成一个随机的合法栈操作序列
+// 给定用来表示输入和输出的元素
+// 这个算法来自高德纳《计算机程序设计艺术》第四卷，7.2.1.6
+// 关于其设计思路，请参见卡塔兰数的扩展版本：卡塔兰三角的递推公式
+// 它用来在栈这一部分生成随机测试样例
+template <typename T>
+class RandomStackOperation : public clazy_framework::Algorithm<vector<T>, int, const T&, const T&> {
+public:
+    vector<T> apply(int n, const T& push, const T& pop) override {
+        vector<T> v(n);
+        int p = n, q = n;
+        for (int i = 0; i < n * 2; i++) {
+            int r = clazy_framework::Random::nextIntBetween(0, (p+q) * (q-p+1));
+            if (r < p * (q-p+2)) {
+                v[i] = push;
+                p--;
+            } else {
+                v[i] = pop;
+                q--;
+            }
+        }
+        return v;
+    }
+};
+
+// // 生成一个随机的栈操作序列
+// template <typename T, typename Vector = clazy::Vector<T>>
+// requires (is_base_of_v<AbstractVector<T>, Vector>)
+// Vector randomStackOperation(int n, T push_e, T pop_e) {
+//     int p = n, q = n;
+//     Vector V;
+//     for (int i : views::iota(0, n * 2)) {
+//         int r = Random::nextIntBetween(0, (p+q) * (q-p+1));
+//         if (r < p * (q-p+2)) {
+//             V.push_back(push_e); p--;
+//         } else {
+//             V.push_back(pop_e); q--;
+//         }
+//     }
+//     return V;
+// }
+
+}
--- headers/chap01/random_framework.h
+++ headers/chap01/random_framework.h
@@ -1,11 +1,10 @@
 #pragma once
 
 // 这个文件用于生成测试用的随机数据
+
 #include <random>
-#include <vector>
 #include <ctime>
-#include "vector.h"
-#include "stack_framework.h"
+#include "framework.h"
 
 namespace clazy_framework {
 
@@ -17,8 +16,14 @@ private:
 public:
     // 生成(0,1)中的随机数
     static double nextDouble();
+    
     // 生成一个随机的16位无符号整数
-    static unsigned int nextUInt();
+    static uint16_t nextUInt16();
+
+    // 生成一个随机的32位无符号整数
+    static uint32_t nextUInt();
+    
+    // 生成一个[lo,hi)范围内的随机整数
     // 包括lo，不包括hi
     static int nextIntBetween(int lo, int hi);
 };
@@ -27,7 +32,11 @@ double Random::nextDouble() {
     return _doubleDist(_random);
 }
 
-unsigned int Random::nextUInt() {
+uint16_t Random::nextUInt16() {
+    return _intDist(_random);
+}
+
+uint32_t Random::nextUInt() {
     return ((unsigned int)_intDist(_random)) << 16 | (unsigned int)_intDist(_random);
 }
 
@@ -39,33 +48,5 @@ int Random::nextIntBetween(int lo, int hi) {
 default_random_engine Random::_random = default_random_engine(time(0));
 uniform_real_distribution<double> Random::_doubleDist = uniform_real_distribution<double>(0.0, 1.0);
 uniform_int_distribution<int> Random::_intDist = uniform_int_distribution<int>(0, 65535);
-
-// 生成一个随机向量
-template <typename Vector = clazy::Vector<int>>
-requires (is_base_of_v<AbstractVector<int>, Vector>)
-Vector randomVector(int n, int lo = 0, int hi = 65535) {
-    Vector result(n);
-    for (int i : views::iota(0, n)) {
-        result.push_back(Random::nextIntBetween(lo, hi));
-    }
-    return result;
-}
-
-// 生成一个随机的栈操作序列
-template <typename T, typename Vector = clazy::Vector<T>>
-requires (is_base_of_v<AbstractVector<T>, Vector>)
-Vector randomStackOperation(int n, T push_e, T pop_e) {
-    int p = n, q = n;
-    Vector V;
-    for (int i : views::iota(0, n * 2)) {
-        int r = Random::nextIntBetween(0, (p+q) * (q-p+1));
-        if (r < p * (q-p+2)) {
-            V.push_back(push_e); p--;
-        } else {
-            V.push_back(pop_e); q--;
-        }
-    }
-    return V;
-}
 
 }
